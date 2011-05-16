959d537b223e6d22439c001b7385d1ffc1a6fa16
Gabe Anzelini <me@gabeanzelini.com>
2011-05-16 08:58:58 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.beta1'
+
+gem 'bson_ext'
+gem 'mongo_mapper'
+
+gem 'sass'
+gem 'coffee-script'
+gem 'uglifier'
+
+gem 'jquery-rails'
+
+
+
+group :development, :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theodore::Application.config.secret_token = '568f911d04fd3cc4fcc7f455abb927f4f37e9ee10676f16e9dd8a210a8c64289730380fa2f2491daa5fa56459d7e25f710d3b6465de281e89fbfd951637451b4'
